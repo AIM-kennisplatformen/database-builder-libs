d424e7b77fb7ea3229b24d6c0df043ad5785eb04
raoul grouls <Raoul.Grouls@han.nl>
2026-01-15 12:28:32 +0100

    abstract source

---
+++ src/backend/sources/abstract_source.py
@@ -0,0 +1,45 @@
+from abc import ABC
+from datetime import datetime
+from typing import Optional
+from uuid import UUID
+
+from pydantic import BaseModel
+
+
+class Content(BaseModel):
+    date: datetime
+    id_: UUID
+    content: dict
+
+
+class UUIDdata(BaseModel):
+    def __call__(self, artefacts_ref: list[str]) -> list[UUID]:
+        pass
+
+    def check_uuid(self, artefact_ref: str) -> UUID:
+        """checks in the database if an artefact is already present"""
+        pass
+
+
+class AbstractSource(ABC, BaseModel):
+    """
+    connects optionally to the source,
+    retrieves data from the source,
+    optionally preprocesses the data (eg pdf to text, metadata extraction, etc)
+    """
+
+    uuididata: UUIDdata = UUIDdata()
+
+    def connect_to_source(self) -> None:
+        """Abstract method to connect to the source."""
+        pass
+
+    def get_list_artefacts(
+        self, last_synced: Optional[datetime]
+    ) -> list[tuple[UUID, datetime]]:
+        """Abstract method to preprocess data from the source."""
+        pass
+
+    def get_content(self, artefacts: list[UUID, datetime]) -> list[Content]:
+        """Abstract method to get data from the source."""
+        pass
