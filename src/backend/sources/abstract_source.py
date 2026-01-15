fbbc0c336930696d45f540279fbf8b4409d3c4ba
raoul grouls <Raoul.Grouls@han.nl>
2026-01-15 12:31:52 +0100

    abstract source

--- src/backend/sources/abstract_source.py
+++ src/backend/sources/abstract_source.py
@@ -43,3 +43,9 @@ class AbstractSource(ABC, BaseModel):
     def get_content(self, artefacts: list[UUID, datetime]) -> list[Content]:
         """Abstract method to get data from the source."""
         pass
+
+    def __call__(self, last_synced: Optional[datetime]) -> list[Content]:
+        self.connect_to_source()
+        artefacts = self.get_list_artefacts(last_synced)
+        contents = self.get_content(artefacts)
+        return contents
